b69eada5e4f721d611b177dc37374ebd27c839b9
Guido Perman <guidoyaelperman@gmail.com>
2025-06-10 12:59:26 -0300

    Revert "customizable dashboard ready"
    This reverts commit 5022a1482af8e50284f2a077868710d7a71ea973.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,30 +1,11 @@
-import { CommonModule } from '@angular/common';
-import { Component, ElementRef, inject, OnInit, viewChild } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { WidgetComponent } from "../../components/widget/widget.component";
-import { DashboardService } from '../../services/dashboard.service';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { wrapGrid } from 'animate-css-grid';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  standalone: true,
-  imports: [CommonModule, MatGridListModule, MatCardModule, WidgetComponent, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
-  providers: [DashboardService]
+  styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent implements OnInit {
-  store = inject(DashboardService);
+export class DashboardComponent {
 
-  dashboard = viewChild.required<ElementRef>('dashboard');
-
-  ngOnInit(): void {
-    wrapGrid(this.dashboard().nativeElement, {
-      duration: 300
-    });
-  }
 }
--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -3,17 +3,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { CustomSidenavComponent } from '../../components/custom-sidenav/custom-sidenav.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule,
-    RouterOutlet,
+  imports: [RouterOutlet,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
